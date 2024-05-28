b462c18b16ba78d0f6ef464a6df6d8b546024ed8
SMARCOCCIA <seb@gmail.fr>
2024-05-28 16:48:48 +0200

    Ajout d'un logger dans la classe Factorial + Test da la méthode compute pour calculer un factoriel, modification du pattern pour afficher d'autre message

--- src/main/java/fr/maboite/Factorial.java
+++ src/main/java/fr/maboite/Factorial.java
@@ -1,21 +1,34 @@
 package fr.maboite;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Factorial {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(Factorial.class);
+	
 	public Integer compute(int i) {
+		LOGGER.info("Méthode compute dans la classe Factoriel");
+		LOGGER.trace("Début");
 		int res=1;
+		LOGGER.debug("Initialisation de la variable res : {}", res);
 		if(i < 0) {
+			LOGGER.error("Entrée invalide dans la condition i < 0 {} : ", i);
 			return 0;
 		}else if(i == 0 || i == 1) {
+			LOGGER.debug("Dans la condition i==0 || i==1 : {} ", i);
 			return 1;
 		}else {
+			LOGGER.debug("Dans la condition else.");
 			for (int j = i; j > 1; j--) {
+				LOGGER.debug("i : {}", i);
+				LOGGER.debug("j : {}", j);
 				res*=j;
-				System.out.println(res);
+				LOGGER.debug("res : {}", res);
 			}
 		}
 		
+		LOGGER.trace("FIN");
 		return res;
 	}
-
 }
--- src/main/java/fr/maboite/Main.java
+++ src/main/java/fr/maboite/Main.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Main {
 
 	public static void main(String[] args) throws JsonProcessingException {
-		String[] seasons = {"Winter", "Spring", "Summer", "Autumn"};
-		PojoToJSON myPojo = new PojoToJSON("salut", "ça va ?", seasons); 
-		ObjectMapper mapper = new ObjectMapper(); 
-		String jsonString = mapper.writeValueAsString(myPojo);
-		System.out.println(jsonString);
-
+//		String[] seasons = {"Winter", "Spring", "Summer", "Autumn"};
+//		PojoToJSON myPojo = new PojoToJSON("salut", "ça va ?", seasons); 
+//		ObjectMapper mapper = new ObjectMapper(); 
+//		String jsonString = mapper.writeValueAsString(myPojo);
+//		System.out.println(jsonString);
+		
+		Factorial factorial=new Factorial();
+		factorial.compute(3);
 	}
 	
 //	J'ai lancé le main sans les scope test et scope compile cela fonctionne quand même parce que
