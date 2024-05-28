13633875d575ea029cc9794cd2566ddbc25a61f1
Nasser <nasserbrid@gmail.com>
2024-05-28 16:40:38 +0200

    ajout de logger SLF4F dans la classe Factorielle - customisation des messages dans logback.xml

--- src/main/java/fr/maboite/Factorial.java
+++ src/main/java/fr/maboite/Factorial.java
@@ -1,6 +1,15 @@
 package fr.maboite;
 
+
+import fr.maboite.logger.LoggerExample;
+import fr.maboite.logger.Looper;
+import fr.maboite.logger.TestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Factorial {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Factorial.class);
 
 	public Integer compute(int i) {
 		// TODO Auto-generated method stub
@@ -13,5 +22,15 @@ public class Factorial {
 		}
         return fact;
     }
+
+	public static void main(String[] args) throws TestException {
+		LOGGER.info("Démarrage du main");
+		Looper looper = new Looper();
+		LOGGER.info("Le factoriel vaut : {}", looper.loop(10));
+		LOGGER.debug("Le factoriel des entiers négatifs vaut 1 ");
+		LOGGER.error("Le factoriel de zéro vaut 1 ");
+		LOGGER.info("Fin du main");
+
+	}
 
 }
